cbc740092e497e2d4bac7f14dea638248bb36fbd
Jan Dostal <jan.dostal@tul.cz>
2022-12-06 21:19:32 +0100

    Added more materials

--- materialy/test/test.m
+++ materialy/test/test.m
@@ -9,14 +9,24 @@ rgbMatrix(2,2, 1:3) = [0 0 1];
 x1 = 0.5;
 y1 = 0.5;
 
-o = image(x1, y1, rgbMatrix);
-hold on;
+fig = figure();
+
+image(x1, y1, rgbMatrix);
 
 x=[1 2];
 y=[1 0];
 
-
 line(x, y, 'Color', 'black', 'LineWidth', 2.5);
 line(x, [0 1], 'Color', 'black', 'LineWidth', 2.5);
+
 rectangle('Position', [0.25 1.3 0.5 0.5], 'Curvature', [1 1], 'FaceColor', 'r');
+
+rectangle('Position', [1.25 1.3 0.5 0.5], 'Curvature', [1 1], 'FaceColor', 'r');
+
+fig_axes = get(fig, "CurrentAxes");
+set(fig_axes, 'XColor', 'none', 'YColor', 'none');
+
+image(x1, y1, rgbMatrix);
+
+
 
