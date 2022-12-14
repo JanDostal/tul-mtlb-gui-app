ffeacc04057e4e712fbac3d3de385941f223adb6
Jan Dostal <jan.dostal@tul.cz>
2022-12-14 11:28:51 +0100

    Edited materials

---
+++ materialy/test/addon.m
@@ -0,0 +1,3 @@
+
+%ziskavani bodu na hernim planu
+[x, y, button] = ginput(1);
--- materialy/test/drawing_matrix_as_color_image.m
+++ materialy/test/drawing_matrix_as_color_image.m
@@ -1,6 +1,6 @@
 %experiment vykresleni matice jako obrazek s ruznymi barvami
+% vykresleni popisků sloupců a řádků
 %dale vykreslovani tvarů křížek a tečka do obrázku
-%dale schovani souradnicovych os
 %dale reset obrazku na modrou barvu
 %dale vykresleni mrizek mezi polickami obrazku
 
@@ -11,24 +11,24 @@ rgbMatrix(1,2, 1:3) = [0 1 0];
 rgbMatrix(2,1, 1:3) = [1 0 1];
 rgbMatrix(2,2, 1:3) = [0 0 1];
 
-x1 = 0.5;
-y1 = 0.5;
+x1 = 1;
+y1 = 1;
 fig = figure();
 img = image(x1, y1, rgbMatrix);
 
 fig_axes = get(fig, "CurrentAxes");
-set(fig_axes, 'XColor', 'none', 'YColor', 'none');
+yticklabels(["", "A", "", "B"]);
+xticklabels(["", "1", "", "2"]);
+set(fig_axes, 'LineWidth', 5.5);
 
-line([1 1], [2 0], 'Color', 'black', 'LineWidth', 5.5);
-line([0 2], [1 1], 'Color', 'black', 'LineWidth', 5.5);
+line([1.5 1.5], [0.5 2.5], 'Color', 'black', 'LineWidth', 5.5);
+line([0.5 2.5], [1.5 1.5], 'Color', 'black', 'LineWidth', 5.5);
 
-x=[1 2];
-y=[1 0];
-line(x, y, 'Color', 'black', 'LineWidth', 2.5);
-line(x, [0 1], 'Color', 'black', 'LineWidth', 2.5);
+line([1.5 2.5], [1.5 0.5], 'Color', 'black', 'LineWidth', 2.5);
+line([1.5 2.5], [0.5 1.5], 'Color', 'black', 'LineWidth', 2.5);
 
-rectangle('Position', [0.25 1.3 0.5 0.5], 'Curvature', [1 1], 'FaceColor', 'r');
-rectangle('Position', [1.25 1.3 0.5 0.5], 'Curvature', [1 1], 'FaceColor', 'r');
+rectangle('Position', [0.75 0.8 0.5 0.5], 'Curvature', [1 1], 'FaceColor', 'r');
+rectangle('Position', [0.75 1.8 0.5 0.5], 'Curvature', [1 1], 'FaceColor', 'r');
 
 rgbMatrix(1, 1:2, 1:3) = [0 0 1; 0 0 1];
 rgbMatrix(2, 1:2, 1:3) = [0 0 1; 0 0 1];
